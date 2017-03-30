b1fb9429cd8c1220199cbb622661832e83c2dc4d
shrik3 <cshrik3@gmail.com>
2017-03-30 11:17:21 +0800

    api_with_photo_path

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -10,16 +10,25 @@ class CommunityController extends Controller
 {
     //
     public function index(){
-        return view('community/index')->withCommunities(\App\Community::all());
+        //return view('community/index')->withCommunities(\App\Community::all());
+        $results = \App\Community::all();
+        foreach ($results as $result){
+            $icon_path = \App\Community::find($result['id'])->icon['image'];
+            $icon_path = url('images/'.$icon_path.'.jpg');
+            $result['icon'] = $icon_path;
+        }
+        $json = json_encode($results);
+        return str_replace("\\/" , "/" , $json);
+
     }
 
     public function show($id)
     {
         $result = \App\Community::find($id);
         $icon_path = \App\Community::find($id)->icon;
-        $result['icon']= $icon_path['image'];
+        $result['icon']= url('images/'.$icon_path['image'].'.jpg');
         $json = json_encode($result);
-        return $json;
+        return str_replace("\\/" , "/" , $json);
 
     }
 }
